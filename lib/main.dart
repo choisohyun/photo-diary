233420345ab4ff3d94c89f4e234bc3dbd014c5fe
choisohyun <sohyun4283@naver.com>
2023-05-23 08:05:23 +0900

    feat: 달력 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:photodiary/pages/auth_page.dart';
-// import 'package:photodiary/pages/firestore_page.dart';
-// import 'package:photodiary/pages/storage_page.dart';
+import 'package:photodiary/screens/home_screen.dart';
 
 import 'firebase_options.dart';
 
@@ -10,13 +8,6 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
-  // await FirebaseRemoteConfig.instance.setConfigSettings(RemoteConfigSettings(
-  //   fetchTimeout: const Duration(minutes: 1),
-  //   minimumFetchInterval: const Duration(minutes: 1),
-  // ));
-  // await FirebaseRemoteConfig.instance
-  //     .setDefaults(const {"platformString": "Hello!"});
 
   runApp(const MyApp());
 }
@@ -30,61 +21,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _State();
-}
-
-class _State extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Center(
-          child: Text("Flutter Firebase Demo"),
-        ),
-      ),
-      body: Center(
-        child: ListView(
-          children: [
-            Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12),
-                child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                            builder: (context) => const AuthPage()),
-                      );
-                    },
-                    child: const Text("Authentication"))),
-            // Container(height: 10),
-            // Padding(
-            //     padding: const EdgeInsets.symmetric(horizontal: 12),
-            //     child: ElevatedButton(
-            //         onPressed: () {
-            //           Navigator.of(context).push(MaterialPageRoute(
-            //               builder: (context) => const FirestorePage()));
-            //         },
-            //         child: const Text("Cloud Firestore"))),
-            // Container(height: 10),
-            // Padding(
-            //     padding: const EdgeInsets.symmetric(horizontal: 12),
-            //     child: ElevatedButton(
-            //         onPressed: () {
-            //           Navigator.of(context).push(MaterialPageRoute(
-            //               builder: (context) => const StoragePage()));
-            //         },
-            //         child: const Text("Storage"))),
-          ],
-        ),
-      ),
+      home: const HomeScreen(),
     );
   }
 }
